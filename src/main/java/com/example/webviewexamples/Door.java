660d0f9281280497d4af3fb6300bfb619b97173d
imraklr <rklsspty777@gmail.com>
2023-01-09 21:00:31 +0530

    Improve menu items and add more menu items.

--- src/main/java/com/example/webviewexamples/Door.java
+++ src/main/java/com/example/webviewexamples/Door.java
@@ -32,8 +32,11 @@ public class Door extends Application {
     private Rectangle m_strip; // menu_strip
     private Circle new_page, new_page_effect_circle,
             bookmarks, bookmarks_effect_circle,
-            refresh, refresh_effect_circle;
-    private Label new_page_label, bookmarks_label, refresh_label;
+            refresh, refresh_effect_circle,
+            backward, backward_effect_circle,
+            forward, forward_effect_circle,
+            close_page, close_page_effect_circle;
+    private Label new_page_label, bookmarks_label, refresh_label, close_page_label, forward_label, backward_label;
     private Rectangle cover_util;
     private Rectangle minimize, maximize, close;
 
@@ -172,11 +175,56 @@ public class Door extends Application {
             refresh_effect_circle.setId("menu_strip_effect_circle");
             refresh.setId("menu_strip_item_circles");
             refresh_label = new Label("↻");
-            refresh_label.setScaleX(refresh.getRadius() / 5.0);
-            refresh_label.setScaleY(refresh.getRadius() / 6.0);
+            refresh_label.setScaleX(refresh.getRadius() / 6.0);
+            refresh_label.setScaleY(refresh.getRadius() / 7.0);
             pane.getChildren().add(8, refresh_effect_circle);
             pane.getChildren().add(9, refresh_label);
             pane.getChildren().add(10, refresh);
+
+            close_page = new Circle(m_strip.getHeight() / 2.5);
+            close_page.setFill(Color.web("#FFFFFF"));
+            close_page.setOpacity(0.3);
+            close_page_effect_circle = new Circle(m_strip.getHeight() / 2.5);
+            close_page_effect_circle.setFill(Color.web("#FFFFFF"));
+            close_page_effect_circle.setOpacity(0.0);
+            close_page_effect_circle.setId("menu_strip_effect_circle");
+            close_page.setId("menu_strip_item_circles");
+            close_page_label = new Label("✘");
+            close_page_label.setScaleX(close_page.getRadius() / 7.0);
+            close_page_label.setScaleY(close_page.getRadius() / 8.0);
+            pane.getChildren().add(11, close_page_effect_circle);
+            pane.getChildren().add(12, close_page_label);
+            pane.getChildren().add(13, close_page);
+
+            forward = new Circle(m_strip.getHeight() / 2.5);
+            forward.setFill(Color.web("#FFFFFF"));
+            forward.setOpacity(0.3);
+            forward_effect_circle = new Circle(m_strip.getHeight() / 2.5);
+            forward_effect_circle.setFill(Color.web("#FFFFFF"));
+            forward_effect_circle.setOpacity(0.0);
+            forward_effect_circle.setId("menu_strip_effect_circle");
+            forward.setId("menu_strip_item_circles");
+            forward_label = new Label("❱");
+            forward_label.setScaleX(forward.getRadius() / 7.0);
+            forward_label.setScaleY(forward.getRadius() / 8.0);
+            pane.getChildren().add(13, forward_effect_circle);
+            pane.getChildren().add(14, forward_label);
+            pane.getChildren().add(15, forward);
+
+            backward = new Circle(m_strip.getHeight() / 2.5);
+            backward.setFill(Color.web("#FFFFFF"));
+            backward.setOpacity(0.3);
+            backward_effect_circle = new Circle(m_strip.getHeight() / 2.5);
+            backward_effect_circle.setFill(Color.web("#FFFFFF"));
+            backward_effect_circle.setOpacity(0.0);
+            backward_effect_circle.setId("menu_strip_effect_circle");
+            backward.setId("menu_strip_item_circles");
+            backward_label = new Label("❰");
+            backward_label.setScaleX(backward.getRadius() / 7.0);
+            backward_label.setScaleY(backward.getRadius() / 8.0);
+            pane.getChildren().add(16, backward_effect_circle);
+            pane.getChildren().add(17, backward_label);
+            pane.getChildren().add(18, backward);
 
             bookmarks = new Circle(m_strip.getHeight() / 2.5);
             bookmarks.setFill(Color.web("#FFFFFF"));
@@ -189,9 +237,9 @@ public class Door extends Application {
             bookmarks_label = new Label("\uD83D\uDD16");
             bookmarks_label.setScaleX(bookmarks.getRadius() / 10.0);
             bookmarks_label.setScaleY(bookmarks.getRadius() / 10.0);
-            pane.getChildren().add(11, bookmarks_effect_circle);
-            pane.getChildren().add(12, bookmarks_label);
-            pane.getChildren().add(13, bookmarks);
+            pane.getChildren().add(19, bookmarks_effect_circle);
+            pane.getChildren().add(20, bookmarks_label);
+            pane.getChildren().add(21, bookmarks);
 
         }
         return pane;
@@ -210,18 +258,32 @@ public class Door extends Application {
 
         // Do frame = new Dimens(); to start with another framing iff you don't require previous frame or if there are
         // animations/resize/drag available
+        double averageWidth = (((bookmarks.getRadius() + new_page.getRadius() + refresh.getRadius() + close_page.getRadius() +
+                forward.getRadius() + backward.getRadius()) * 2) + 5 * (m_strip.getWidth() / 200)) / 2;
         Dimens frame1 = new Dimens();
-        new Dimens(new_page_effect_circle, frame1, 0, 0, 0, 0, 0, 0, 0, 0, m_strip.getHeight() / 20, m_strip.getWidth() / 2 - new_page.getLayoutBounds().getWidth() / 2, 0, 0);
+        new Dimens(new_page_effect_circle, frame1, 0, 0, 0, 0, 0, 0, 0, 0, m_strip.getHeight() / 20, m_strip.getWidth() / 2 - averageWidth, 0, 0);
         new Dimens(new_page, frame1, 0, 0, 0, 0, 0, 0, 0, 0, m_strip.getHeight() / 20, m_strip.getWidth() / 2 - new_page.getLayoutBounds().getWidth() / 2, 0, 0);
-        new Dimens(new_page_label, frame1, 0, 0, 0, new_page.getRadius() / 1.5, 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(new_page_label, frame1, -new_page.getRadius() / 20, 0, 0, new_page.getRadius() / 1.6, 0, 0, 0, 0, 0, 0, 0, 0);
 
         new Dimens(refresh, frame1, 0, 0, 0, refresh.getLayoutBounds().getWidth() + m_strip.getWidth() / 200, 0, 0, 0, 0, 0, 0, 0, 0);
         new Dimens(refresh_effect_circle, frame1, 0, 0, 0, refresh.getLayoutBounds().getWidth() + m_strip.getWidth() / 200, 0, 0, 0, 0, 0, 0, 0, 0);
-        new Dimens(refresh_label, frame1, 0, 0, 0, refresh.getLayoutBounds().getWidth() + m_strip.getWidth() / 200 + refresh_label.getWidth(), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(refresh_label, frame1, 0, 0, 0, refresh.getLayoutBounds().getWidth() + m_strip.getWidth() / 200 + refresh_label.getWidth() / 0.8, 0, 0, 0, 0, 0, 0, 0, 0);
 
         new Dimens(bookmarks, frame1, 0, 0, 0, bookmarks.getLayoutBounds().getWidth() + 5 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
         new Dimens(bookmarks_effect_circle, frame1, 0, 0, 0, bookmarks.getLayoutBounds().getWidth() + 5 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
-        new Dimens(bookmarks_label, frame1, 0, 0, 0, bookmarks.getLayoutBounds().getWidth() + 5 * (m_strip.getWidth() / 200) + bookmarks_label.getWidth() / 2.5, 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(bookmarks_label, frame1, bookmarks.getRadius() / 20, 0, 0, bookmarks.getLayoutBounds().getWidth() + 5 * (m_strip.getWidth() / 200) + bookmarks_label.getWidth() / 2.5, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        new Dimens(close_page, frame1, 0, 0, 0, close_page.getLayoutBounds().getWidth() + 9 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(close_page_effect_circle, frame1, 0, 0, 0, close_page.getLayoutBounds().getWidth() + 9 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(close_page_label, frame1, close_page.getRadius() / 20, 0, 0, close_page.getLayoutBounds().getWidth() + 9.5 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+
+        new Dimens(forward, frame1, 0, 0, 0, forward.getLayoutBounds().getWidth() + 13 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(forward_effect_circle, frame1, 0, 0, 0, forward.getLayoutBounds().getWidth() + 13 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(forward_label, frame1, forward.getRadius() / 20, 0, 0, forward.getLayoutBounds().getWidth() + 14.25 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+
+        new Dimens(backward, frame1, 0, 0, 0, backward.getLayoutBounds().getWidth() + 17 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(backward_effect_circle, frame1, 0, 0, 0, backward.getLayoutBounds().getWidth() + 17 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
+        new Dimens(backward_label, frame1, backward.getRadius() / 20, 0, 0, backward.getLayoutBounds().getWidth() + 18 * (m_strip.getWidth() / 200), 0, 0, 0, 0, 0, 0, 0, 0);
 
         frame1.arrange();
     }
@@ -288,6 +350,60 @@ public class Door extends Application {
             fadeTransition2.setFromValue(refresh_effect_circle.getOpacity());
             fadeTransition2.setToValue(.2);
             fadeTransition2.play();
+        });
+        FadeTransition fadeTransition3 = new FadeTransition(Duration.seconds(.125), close_page_effect_circle);
+        close_page.setOnMouseEntered(e -> {
+            fadeTransition3.setFromValue(close_page_effect_circle.getOpacity());
+            fadeTransition3.setToValue(1.);
+            fadeTransition3.play();
+        });
+        close_page.setOnMouseClicked(e -> {
+            // Perform some action and opacity restoration
+            fadeTransition3.setFromValue(close_page_effect_circle.getOpacity());
+            fadeTransition3.setToValue(.2);
+            fadeTransition3.play();
+        });
+        close_page.setOnMouseExited(e -> {
+            // Perform opacity restoration
+            fadeTransition3.setFromValue(close_page_effect_circle.getOpacity());
+            fadeTransition3.setToValue(.2);
+            fadeTransition3.play();
+        });
+        FadeTransition fadeTransition4 = new FadeTransition(Duration.seconds(.125), forward_effect_circle);
+        forward.setOnMouseEntered(e -> {
+            fadeTransition4.setFromValue(forward_effect_circle.getOpacity());
+            fadeTransition4.setToValue(1.);
+            fadeTransition4.play();
+        });
+        forward.setOnMouseClicked(e -> {
+            // Perform some action and opacity restoration
+            fadeTransition4.setFromValue(forward_effect_circle.getOpacity());
+            fadeTransition4.setToValue(.2);
+            fadeTransition4.play();
+        });
+        forward.setOnMouseExited(e -> {
+            // Perform opacity restoration
+            fadeTransition4.setFromValue(forward_effect_circle.getOpacity());
+            fadeTransition4.setToValue(.2);
+            fadeTransition4.play();
+        });
+        FadeTransition fadeTransition5 = new FadeTransition(Duration.seconds(.125), backward_effect_circle);
+        backward.setOnMouseEntered(e -> {
+            fadeTransition5.setFromValue(backward_effect_circle.getOpacity());
+            fadeTransition5.setToValue(1.);
+            fadeTransition5.play();
+        });
+        backward.setOnMouseClicked(e -> {
+            // Perform some action and opacity restoration
+            fadeTransition5.setFromValue(backward_effect_circle.getOpacity());
+            fadeTransition5.setToValue(.2);
+            fadeTransition5.play();
+        });
+        backward.setOnMouseExited(e -> {
+            // Perform opacity restoration
+            fadeTransition5.setFromValue(backward_effect_circle.getOpacity());
+            fadeTransition5.setToValue(.2);
+            fadeTransition5.play();
         });
     }
 
